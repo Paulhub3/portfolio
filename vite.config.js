15c99d785d10d78a499f46dbfaa9a65f502bf9ef
Paul <somtopaul235@gmail.com>
2023-04-09 07:15:50 -0700

    yy

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["/images/profile.png"],
-    },
+  rollupOptions: {
+    external: ["/images/profile.png"],
   },
   plugins: [vue(), vueJsx()],
   resolve: {
